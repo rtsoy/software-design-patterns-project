4834507094ef46f26853b0631cb7448b8520ad3b
rtsoy <tsoiroman98@gmail.com>
2023-10-15 16:03:50 +0600

    chore(postgres): add comments and documentation

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -5,12 +5,14 @@ import (
 	"gorm.io/gorm"
 )
 
+// New initializes a new PostgreSQL database connection and performs auto-migration for the provided models.
 func New(dsn string, models []interface{}) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		return nil, err
 	}
 
+	// Auto-migrate each provided model to create corresponding database tables.
 	for _, model := range models {
 		err = db.AutoMigrate(model)
 		if err != nil {
